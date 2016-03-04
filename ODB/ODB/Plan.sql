9b2a3f190b69a4bb453649af391a2353471bc13c
Vlasovets <otorrent@mail.ru>
2016-03-05 00:50:54 +0300

    Data_Base

---
+++ ODB/ODB/Plan.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Plan] ( 
+[User_login] VARCHAR (50) NOT NULL, 
+[Description] VARCHAR (140) NOT NULL, 
+[IsCompleted] VARCHAR (1) NOT NULL, 
+PRIMARY KEY CLUSTERED ([Description] ASC), 
+CONSTRAINT [User_login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[User] ([Login]) 
+);
