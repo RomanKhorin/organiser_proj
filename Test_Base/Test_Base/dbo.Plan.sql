de9138af4bb93f138760af179f0cbebb324b50f2
Vlasovets <otorrent@mail.ru>
2016-03-04 18:19:54 +0300

    Organiser_DB

---
+++ Test_Base/Test_Base/dbo.Plan.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Plan]
+(
+	[description] VARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [is_completed] BIT NOT NULL, 
+    CONSTRAINT [FK_Plan_User] FOREIGN KEY ([description]) REFERENCES [User]([login])
+)
---
+++ Test_Base/Test_Base/dbo.User_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[login] VARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NOT NULL, 
+    [surname] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL, 
+    [date_of_birth] DATE NOT NULL
+)
