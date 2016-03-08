ccc47e1968e4410929f4deb20036d1287ec34207
Vlasovets <otorrent@mail.ru>
2016-03-08 21:21:52 +0300

    updated_DBO

---
+++ ODB/ODB/Plan_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Plan] ( 
+[User_login] VARCHAR (50) NOT NULL, 
+[Description] VARCHAR (140) NOT NULL, 
+[IsCompleted] VARCHAR (1) NOT NULL, 
+[DeadLine] DATETIME NOT NULL, 
+CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED ([Description] ASC, [DeadLine] ASC), 
+CONSTRAINT [User_login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[User] ([Login]) 
+);
