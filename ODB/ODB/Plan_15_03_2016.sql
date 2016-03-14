5bc22842e6b0ccfa7b338f85fd87999937607682
Vlasovets <otorrent@mail.ru>
2016-03-15 00:41:56 +0300

    15_03_2016

--- ODB/ODB/Plan_15_03_2016.sql
+++ ODB/ODB/Plan_15_03_2016.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE [dbo].[Plan] ( 
+﻿CREATE TABLE [dbo].[Plan_15_03_2016] ( 
+[Id] INT IDENTITY (1, 1) NOT NULL, 
 [User_login] VARCHAR (50) NOT NULL, 
 [Description] VARCHAR (140) NOT NULL, 
 [IsCompleted] VARCHAR (1) NOT NULL, 
-[DeadLine] DATETIME NOT NULL, 
+[DeadLine] DATE NOT NULL, 
 [Date] DATETIME NOT NULL, 
-CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED ([Date] ASC, [Description] ASC), 
+CONSTRAINT [PK_Plan_15_03_2016] PRIMARY KEY CLUSTERED ([Date] ASC, [Id] ASC), 
 CONSTRAINT [User_login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[User] ([Login]) 
 );
