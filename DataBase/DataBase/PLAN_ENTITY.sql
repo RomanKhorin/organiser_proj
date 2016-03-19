c9e5ce39489009e23d0b689eb1f0517a1dce9550
Vlasovets <otorrent@mail.ru>
2016-03-19 16:23:08 +0300

    new base uploaded

---
+++ DataBase/DataBase/PLAN_ENTITY.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PLAN_ENTITY] ( 
+[Id] INT IDENTITY (1, 1) NOT NULL, 
+[User_login] VARCHAR (50) NOT NULL, 
+[Description] VARCHAR (140) NOT NULL, 
+[IsCompleted] VARCHAR (1) NOT NULL, 
+[DeadLine] DATE NOT NULL, 
+[Date] DATETIME NOT NULL, 
+CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED ([Date] ASC, [Id] ASC), 
+CONSTRAINT [User_login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[USER_ENTITY] ([Login]) 
+);
