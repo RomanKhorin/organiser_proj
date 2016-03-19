1841b8f49de06ebc3ccafad3d78c944463625d30
Vlasovets <otorrent@mail.ru>
2016-03-19 13:33:52 +0300

    let it run

---
+++ ODB/ODB/Plan_entity.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Plan_entity] ( 
+[Id] INT IDENTITY (1, 1) NOT NULL, 
+[User_login] VARCHAR (50) NOT NULL, 
+[Description] VARCHAR (140) NOT NULL, 
+[IsCompleted] VARCHAR (1) NOT NULL, 
+[DeadLine] DATE NOT NULL, 
+[Date] DATETIME NOT NULL, 
+CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED ([Date] ASC, [Id] ASC), 
+CONSTRAINT [Login] FOREIGN KEY ([User_login]) REFERENCES [dbo].[User] ([Login]) 
+);
---
+++ ODB/ODB/User_entity.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User_entity] ( 
+[Login] VARCHAR (50) NOT NULL, 
+[Password] VARCHAR (10) NOT NULL, 
+[Name] VARCHAR(50) NOT NULL, 
+[Surname] VARCHAR(50) NOT NULL, 
+[Date_of_birth] DATETIME NOT NULL, 
+PRIMARY KEY CLUSTERED ([Login] ASC) 
+);
